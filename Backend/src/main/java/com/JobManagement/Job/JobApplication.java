5aed7e32ea81e61607ae4e9164c6402d5f91eb92
Rohit Bhagwan Patil <43942305+skull7t@users.noreply.github.com>
2024-02-20 10:19:30 +0530

    Update JobApplication.java

--- Backend/src/main/java/com/JobManagement/Job/JobApplication.java
+++ Backend/src/main/java/com/JobManagement/Job/JobApplication.java
@@ -3,7 +3,7 @@ package com.JobManagement.Job;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class JobApplication {
 
 	public static void main(String[] args) {
